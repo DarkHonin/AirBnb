57c4b4ca01d00153957aff8322b8f8a956955373
Amokgohl <45788338+DeadMan-Moc@users.noreply.github.com>
2019-04-10 10:23:01 +0200

    Add files via upload
    Just a connection some of the staff I am still testing them.

---
+++ connection/coonnect.js
@@ -0,0 +1,57 @@
+// var MongoClient = require('mongodb').MongoClient, assert = require('assert');
+
+// // Connection URL
+// var url = 'mongodb+srv://allie:Moknare95@airbnb-ttiyl.mongodb.net/test?retryWrites=true'
+// MongoClient.connect(url,{ useNewUrlParser: true });
+// // Use connect method to connect to the Server
+// MongoClient.connect(url, function(err, db) {
+//   useNewUrlParser: true;
+//   assert.equal(null, err);
+//   console.log("Connected correctly to server");
+
+//   db.close();
+// });
+
+// var MongoClient = require('mongodb').MongoClient
+// MongoClient.mongoConnect = () => {
+//   mongoose.Promise = global.Promise
+//   mongoose.connect('mongodb+srv://allie:Moknare95@airbnb-ttiyl.mongodb.net/test?retryWrites=true', {useNewUrlParser: true})
+//   .then(() => {
+//   console.log('mongoDB is connected...')
+//   })
+//   .catch((err) => {
+//   throw err
+//   })
+//   }
+
+// var MongoClient = require('mongodb').MongoClient,format = require('util').format;
+
+// MongoClient.connect('', { useNewUrlParser: true }, function(err,db){
+//   if(err){
+//     throw err;
+//   } else{
+//     console.log("Good");
+//   }
+//   db.close();
+// })
+
+// const mongoose = require("mongoose");
+// mongoose.Promise = Promise;
+// const session = require('express-session');
+// const MongoStore = require('connect-mongo')(session);
+
+// mongoose.connect('mongodb://localhost/air_db');
+
+// app.use(session({
+//     secret: "",
+//     store: new MongoStore({ mongooseConnection: mongoose.connection })
+// }));
+
+var MongoClient = require('mongodb').MongoClient;
+
+var uri = "mongodb+srv://allie:Moknare95@airbnb-ttiyl.mongodb.net/test?retryWrites=true";
+MongoClient.connect(uri, { useNewUrlParser: true } ,function(err, client) {
+   const collection = client.db("air_db").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
