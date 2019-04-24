f281ff9cbd22c9871968e18d85ef4b8ab5156cd4
cedrickmbuyamba <40019362+cedrickmbuyamba@users.noreply.github.com>
2019-04-24 14:27:53 +0200

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+    google: {
+        clientID:'105762379829-25enp505bh53ulbrdrpoelfqauub5uc0.apps.googleusercontent.com',
+        clientSecret:'gJDkeZ6fBtYZyDz6aNL9-tEl'
+
+    },
+    mongodb: process.env.mongo_srv
+};
