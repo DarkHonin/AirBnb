a965576a2545817a88ceda7fd6e917ea1957bf31
DGmoN <darkhonin@gmail.com>
2019-05-29 03:02:32 +0200

    Basic ui fixes

--- aspect.js
+++ aspect.js
@@ -1,6 +1,8 @@
 const BaseAspect = require("../framework/src/base_aspect.js")
+const loader = require("../framework/src/loader.js")
 const express = require("express")
 const path = require('path');
+const users = require("../users/aspect.js")
 var exphbs  = require('express-handlebars');
 
 const router = express.Router()
@@ -10,16 +12,21 @@ router.get("/handlebars", (req, res) =>{
     res.sendFile(path.join(app_root+"/node_modules/handlebars/dist/handlebars.runtime.js"));
 })
 
-router.get("/", (req,res) => {
+router.get("/", loader.expectsAspect("BitwaspAirBnB_Users"), (req,res) => {
+    console.log(req.user)
     res.render("index")
-  });
+});
+
+router.get("/login", loader.expectsAspect("BitwaspAirBnB_Users"), (req, res) =>{
+    res.render("login", {google_id : req.BitwaspAirBnB_Users.secrets.GOOGLE_CLIENT_ID})
+})
 
 module.exports = class UI extends BaseAspect{
     static AspectAuthor(){return "wgourley"}
     static AspectName(){return "BitwaspAirBnB_UI"}
     constructor(app){
         super(app)
-        app.use('/static', express.static('static'))
+        app.use('/static', express.static('assets'))
         app.engine('handlebars', exphbs({defaultLayout: 'main'}));
         app.set('view engine', 'handlebars');
         app.use("/", router)
